deba18ffbb28e97477811553cab5deb60e41438c
Daniil <dprilepskijjj2012@mail.ru>
2024-04-09 21:11:55 +0300

    Initial commit

---
+++ src/main/java/txt/file/Main.java
@@ -0,0 +1,64 @@
+package txt.file;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader inputBR = null;
+        BufferedReader tempBR = null;
+
+        try {
+
+            File inputFile = new File("toUpperCase.txt");
+            if (!inputFile.exists()) {
+                inputFile.createNewFile();
+            }
+            PrintWriter inputPW = new PrintWriter(inputFile);
+            inputPW.println("Java is a programming language and computing platform first released by Sun Microsystems in 1995.\n" +
+                    "It has evolved from humble beginnings to power a large share of today’s digital world,\n" +
+                    "by providing the reliable platform upon which many services and applications are built.\n" +
+                    "New, innovative products and digital services designed for the future continue to rely on Java, as well.");
+            inputPW.close();
+
+
+            File tempFile = new File("tempFile.txt");
+            if (!tempFile.exists()) {
+                tempFile.createNewFile();
+            }
+            PrintWriter tempPW = new PrintWriter(tempFile);
+
+
+            String line;
+
+            inputBR = new BufferedReader(new FileReader("toUpperCase.txt"));
+            System.out.println("\nbefore working with txt file:");
+            while ((line = inputBR.readLine()) != null) {
+                System.out.println(line);
+                tempPW.println(line.toUpperCase());
+            }
+            tempPW.close();
+
+
+            tempBR = new BufferedReader(new FileReader("tempFile.txt"));
+            System.out.println("\nafter working with txt file:");
+            while ((line = tempBR.readLine()) != null) {
+                System.out.println(line);
+            }
+
+
+            inputFile.deleteOnExit();
+            tempFile.renameTo(inputFile);
+
+            
+        } catch (IOException e) {
+            System.out.println("The file cannot be created! Error: " + e);
+        } finally {
+            try {
+                if (inputBR != null) inputBR.close();
+                if (tempBR!= null) tempBR.close();
+            } catch (IOException e) {
+                System.out.println("Error: " + e);
+            }
+        }
+    }
+}
